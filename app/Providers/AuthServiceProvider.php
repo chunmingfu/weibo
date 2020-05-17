e73feb3c2ff110a37d90df8580942126c4ec7d0f
fleming <chunmingfu@outlook.com>
2020-05-17 11:14:54 +0000

    访问策略

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\User::class => \App\Policies\UserPolicy::class,
     ];
 
     /**
@@ -24,7 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        // // 修改策略自动发现的逻辑
+        // Gate::guessPolicyNamesUsing(function ($modelClass) {
+        //     // 动态返回模型对应的策略名称，如：// 'App\Models\User' => 'App\Policies\UserPolicy',
+        //     return 'App\Policies\\'.class_basename($modelClass).'Policy';
+        // });
     }
 }
